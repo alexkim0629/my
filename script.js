18c049f2f78d61e40c3e0b6f4eb4708c46e9b036
Alex Kim <kim17alex@gmail.com>
2026-02-04 01:32:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -44,10 +44,10 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "Try again",
+    "Really??",
+    "...",
+    "Yes",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
